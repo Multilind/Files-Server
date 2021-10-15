6a081816e0a423148001d3b2f8135a0faae53830
Pedro <pedroigor.silva@outlook.com>
2021-10-15 00:06:38 -0300

    feat: verify if the file was sent

--- src/controllers/File/createFile.js
+++ src/controllers/File/createFile.js
@@ -2,7 +2,11 @@ import bucket from "../../config/storage";
 export async function create(req, res) {
   const { params, files } = req;
   const { id_palavra } = params;
-  const { file } = files;
+  const file = files?.file;
+  if (!file) {
+    res.status(401).send("No file sent.");
+    return;
+  }
   const blob = bucket.file(`${id_palavra}/${file.name}`);
   const blobWriter = blob.createWriteStream({
     metadata: {
